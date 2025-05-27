511d1a29b808ff3628af43dd79743fa98877ea4c
Celeste Tanaka <cehmayumi@gmail.com>
2025-05-27 09:36:38 -0300

    :wrench: chore: initial setup of dbcontext

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+var connectionString = builder.Configuration.GetConnectionString("OracleDbContext");
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
