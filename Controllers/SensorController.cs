7db12426c469e6e6d8b5abc71ad63d53626f6678
Celeste Tanaka <cehmayumi@gmail.com>
2025-06-07 00:18:36 -0300

    :sparkles: feat(sensor): integrate ESP32 weather sensor with POST endpoint

---
+++ Controllers/SensorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using climby.DTOs;
+
+namespace climby.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SensorController : ControllerBase
+    {
+        [HttpPost("sensor-weather")]
+        public IActionResult ReceiveSensorWeather([FromBody] SensorWeatherDataDto sensorData)
+        {
+            string alertMessage = GenerateAlert(sensorData);
+
+            var result = new WeatherInfoDto
+            {
+                City = sensorData.City,
+                Temperature = sensorData.Temperature,
+                Description = "Dados do sensor",
+                Icon = "", 
+                AlertMessage = alertMessage
+            };
+
+            return Ok(result);
+        }
+
+        private string GenerateAlert(SensorWeatherDataDto data)
+        {
+            if (data.RainLevel > 50)
+                return "Alerta de chuva forte!";
+            else if (data.Temperature > 38)
+                return "Alerta de calor extremo!";
+            else
+                return null;
+        }
+    }
+}
---
+++ DTOs/SensorDataWeatherDto.cs
@@ -0,0 +1,9 @@
+﻿namespace climby.DTOs
+{
+    public class SensorWeatherDataDto
+    {
+        public string City { get; set; }
+        public decimal Temperature { get; set; }
+        public decimal RainLevel { get; set; }
+    }
+}
